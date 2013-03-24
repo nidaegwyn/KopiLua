b5aba78c7c61a3f3e11dda325bfb290f2ec976ec
Vinicius Jarina <viniciusjarina@gmail.com>
2013-03-24 01:07:03 -0300

    Fixed Warning (iOS)

--- src/ios/AssemblyInfo.cs
+++ src/ios/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
@@ -12,7 +13,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("nlua.org")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+[assembly: CLSCompliantAttribute (true)]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
